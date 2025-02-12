7ec5e75bc86b02d6e38696e7f50a1cf0de836a9b
darkross <andresarcangel00@gmail.com>
2025-02-11 22:40:20 -0500

    Filtrado y Ordenamiento de Table

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,15 +2,32 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatTableModule,MatButtonModule,MatIconModule,MatToolbarModule,MatSidenavModule,MatMenuModule,MatDividerModule,NoopAnimationsModule]
+  exports: [
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatDividerModule,
+    NoopAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule
+  ],
 })
 export class MaterialModule {}
--- src/app/pages/patient/patient.component.ts
+++ src/app/pages/patient/patient.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Patient } from 'src/app/model/patient';
 import { PatientService } from 'src/app/service/patient.service';
+
 
 @Component({
   selector: 'app-patient',
@@ -9,7 +12,9 @@ import { PatientService } from 'src/app/service/patient.service';
   styleUrls: ['./patient.component.css'],
 })
 export class PatientComponent implements OnInit {
+  //definimos el objeto la cual se llenara la tabla
   dataSource: MatTableDataSource<Patient>;
+  //definimos las columnas de la tabla
   displayedColumns: string[] = [
     'id',
     'firstName',
@@ -17,11 +22,23 @@ export class PatientComponent implements OnInit {
     'dni',
     'actions',
   ];
+
+  //@ViewChild => captura un componente del html
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort:MatSort;
+  //agregamos la inyección del metodo obtenido del services
   constructor(private patientService: PatientService) {}
 
   ngOnInit(): void {
     this.patientService.findAll().subscribe((data) => {
       this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     });
   }
+//acción del filtro
+  applyFilter(e: any) {
+    this.dataSource.filter = e.target.value.trim(); 
+  }
+
 }
