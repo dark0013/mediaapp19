73a012892675597436ec0e75a1ff0469ec1079f7
darkross <andresarcangel00@gmail.com>
2025-02-18 08:07:28 -0500

    se implemnto la funcionalidad del delete y el uso reactivo de los registro de paciente

--- src/app/pages/patient/patient.component.ts
+++ src/app/pages/patient/patient.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { switchMap } from 'rxjs';
 import { Patient } from 'src/app/model/patient';
 import { PatientService } from 'src/app/service/patient.service';
 
@@ -51,8 +52,22 @@ export class PatientComponent implements OnInit {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
+
+delete(idPatient:number){
+  this.patientService.delete(idPatient).pipe(switchMap(_=>{
+    return this.patientService.findAll()
+  }))
+  .subscribe(data=>{
+    this.patientService.setPatientChange(data);
+  })
+}
+
   mostrarAlerta() {
     alert('vale');
     console.log('vale');
   }
+
+
+
+
 }
